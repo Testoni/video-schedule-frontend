2dac0b6832958f3c1ea3017a9799925b3e1ab2fe
Gabriel Testoni <gabriel.testoni@gmail.com>
2020-07-27 15:00:35 -0300

    create models

---
+++ src/app/shared/model/video.model.ts
@@ -0,0 +1,9 @@
+export class Video {
+    id: string;
+    videoName: string;
+    channelName: string;
+    videoDate: string;
+    videoTime: string;
+    videoLink: string;
+    registrationDate: string;
+}
